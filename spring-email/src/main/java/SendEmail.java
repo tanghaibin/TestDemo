942d6a252c2b5a87e74a657eaf65c20f3ff36a45
tanghaibin <tanghaibinhaohan@163.com>
2016-05-12 08:45:46 +0800

    新增发送邮件demo

---
+++ spring-email/src/main/java/SendEmail.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+/**
+ * Created by tangh on 2016/5/12.
+ */
+public class SendEmail {
+
+    public static void main(String [] args){
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SimpleMailMessage simpleMailMessage = applicationContext.getBean(SimpleMailMessage.class);
+        MailSender mailSender = applicationContext.getBean(MailSender.class);
+        mailSender.send(simpleMailMessage);
+    }
+}
