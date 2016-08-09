1968b574ae91148cd59cb326694c4b24980598d5
tanghaibin <tanghaibinhaohan@163.com>
2016-08-09 17:46:46 +0800

    commit

--- dubbo-demo-provider/src/main/java/top/tanghaibin/app/Main.java
+++ dubbo-demo-provider/src/main/java/top/tanghaibin/app/Main.java
@@ -2,21 +2,23 @@ package top.tanghaibin.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by tanghaibin on 2016/8/9.
  */
 public class Main {
 
-    public static void main(String [] args){
+    public static void main(String [] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/provider.xml");
-        while(true){
-            try {
-                System.out.println("服务中");
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+        context.start();
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
