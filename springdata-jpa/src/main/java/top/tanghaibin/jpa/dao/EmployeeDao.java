b1cad5314fc7e3c560031fd0e1b6247bf6ff42ce
tanghaibin <tanghaibinhaohan@163.com>
2016-09-26 11:03:07 +0800

    update springdata jpa demo

--- springdata-jpa/src/main/java/top/tanghaibin/jpa/dao/EmployeeDao.java
+++ springdata-jpa/src/main/java/top/tanghaibin/jpa/dao/EmployeeDao.java
@@ -1,10 +1,11 @@
 package top.tanghaibin.jpa.dao;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import top.tanghaibin.jpa.entity.Employee;
 
 /**
  * Created by tanghaibin on 2016/9/21.
  */
-public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+public interface EmployeeDao extends JpaSpecificationExecutor,CrudRepository<Employee,Integer> {
 }
--- springdata-jpa/src/main/java/top/tanghaibin/jpa/service/EmployeeService.java
+++ springdata-jpa/src/main/java/top/tanghaibin/jpa/service/EmployeeService.java
@@ -3,13 +3,22 @@ package top.tanghaibin.jpa.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import top.tanghaibin.jpa.dao.EmployeeDao;
 import top.tanghaibin.jpa.entity.Department;
 import top.tanghaibin.jpa.entity.Employee;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by tanghaibin on 2016/9/21.
@@ -30,6 +39,27 @@ public class EmployeeService {
             System.out.println(iterator.next());
         }
     }
+
+    @Test
+    public void findByExample(){
+
+        Specification<Employee> specification = new Specification<Employee>() {
+            List<Predicate> list = new ArrayList<Predicate>();
+            public Predicate toPredicate(Root<Employee> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+
+                list.add(criteriaBuilder.between(root.get("id").as(Integer.class), 1, 3));
+//                list.add(criteriaBuilder.equal(root.get("department").get("id").as(Integer.class),2));
+                Predicate predicate =  criteriaBuilder.and(list.toArray(new Predicate[list.size()]));
+                Predicate predicate1 = criteriaBuilder.or(criteriaBuilder.equal(root.get("name").as(String.class), "小红"));
+
+                criteriaQuery.orderBy(criteriaBuilder.desc(root.get("id")));
+                return criteriaBuilder.or(new Predicate[]{predicate, predicate1});
+            }
+        };
+
+        System.out.println(employeeDao.findAll(specification));
+    }
+
 
     @Test
     public void save(){
