8849ee85d8d82edfdce850e3b8ad7d31100af547
tanghaibin <tanghaibinhaohan@163.com>
2016-10-20 22:49:30 +0800

    add regex

--- regex-demo/src/main/java/top/tanghaibin/regex/RegexDemo.java
+++ regex-demo/src/main/java/top/tanghaibin/regex/RegexDemo.java
@@ -1,5 +1,7 @@
 package top.tanghaibin.regex;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by 唐海斌 on 2016/8/29.
  */
@@ -8,14 +10,19 @@ public class RegexDemo {
 
     public static void test(String regexStr,String regex){
 
-        boolean b = regexStr.matches(regex);
-        System.out.println(b);
+        System.out.println(Pattern.matches(regex, regexStr));
+
+
     }
 
     public static void main(String [] args){
-        String email = "tanghaibinhaohan@163.com";
-        String phone = "";
-
-        test(email,"^([a-z]+)");
+        String phone = "18682668023rf*";
+        test(phone,"^[\\d]{11}.[\\D]{2}.*");
+        String url = "www.baidu.com";
+        test(url,"^[w]{3}.*[.]{1}.*[a-z]{5}.*[.]{1}.*[a-z]{3}");
+        String name = "tanghaibin";
+        test(name, "^[a-zA-Z0-9]{1,10}");
+        String email = "tanghaibinhaohan@163.net";
+        test(email, "^[a-zA-Z0-9]+.*[@]{1}.*[\\D\\d]{5}.*[com|cn|net]$");
     }
 }
